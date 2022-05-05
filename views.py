b384a6334c608710bbb89b6b0c426c1f819ad43e
SawshaDev <contactcallimarie@gmail.com>
2022-05-05 06:48:46 +0000

    added domains view | not used yet

--- views.py
+++ views.py
@@ -6,3 +6,8 @@ views = Blueprint('views', __name__, template_folder='templates')
 @views.route("/", methods=['GET'])
 def home():
     return render_template("home/home.html")
+
+@views.route('/domains', methods=['GET'])
+def domains():
+    return render_template("etc/domains.html")
+
